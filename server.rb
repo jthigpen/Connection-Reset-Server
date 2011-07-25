c0d5150ef89f40b6e10789b18a2f17b3e27d11c2
James Thigpen <james.r.thigpen@gmail.com>
2011-07-25 16:51:01 -0700

    initial checkin

---
+++ server.rb
@@ -0,0 +1,14 @@
+require 'socket'
+
+dts = TCPServer.new('localhost', 8080)
+
+loop do
+  Thread.start(dts.accept) do |s|
+    print(s, " is accepted\n")
+    s.write("HTTP/1.0 200 OK\n")
+    linger = [1,0].pack('ii')
+    s.setsockopt(Socket::SOL_SOCKET, Socket::SO_LINGER, linger)
+    s.close
+    s.shutdown
+  end
+end
